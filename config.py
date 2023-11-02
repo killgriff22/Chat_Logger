e023ba49a5d5e6cc3462ce8584648b7cf8acd06a
killgriff22 <kilgrif22033@gmail.com>
2023-11-02 15:52:12 -0400

    Added Name support for parser

--- config.py
+++ config.py
@@ -2,28 +2,41 @@ import discord
 import os
 import datetime
 client = discord.Client()
-TOKEN = ""
+TOKEN = "OTc2NTEyNjc0ODA2NTAxMzk3.GacmJv.AHgHT3BjmimpY3yZpxSLRtCJFoiowKV7gNqHkE"
 
 
-def fetch_guild(id):
-    if str(id) in os.listdir("guilds"):
-        return os.listdir(f"guilds/{str(id)}")
-    else:
-        os.mkdir(f"guilds/{str(id)}")
-        return os.listdir(f"guilds/{str(id)}")
+class guilds:
+    def fetch_guild(id, name):
+        if str(id) in os.listdir("guilds"):
+            return os.listdir(f"guilds/{str(id)}")
+        else:
+            os.mkdir(f"guilds/{str(id)}")
+            with open(f"guilds/{str(id)}/name", "w") as f:
+                f.write(name)
+            return os.listdir(f"guilds/{str(id)}")
+
+    def fetch_guilds():
+        return os.listdir("guilds")
 
 
-def fetch_channel(gid, id):
-    if str(id) in fetch_guild(gid):
-        return os.listdir(f"guilds/{gid}/{str(id)}")
-    else:
-        os.mkdir(f"guilds/{gid}/{str(id)}")
-        return os.listdir(f"guilds/{gid}/{str(id)}")
+class channels:
+    def fetch_channel(gid, id, name):
+        if str(id) in guilds.fetch_guild(gid):
+            return os.listdir(f"guilds/{gid}/{str(id)}")
+        else:
+            os.mkdir(f"guilds/{gid}/{str(id)}")
+            with open(f"guilds/{gid}/{str(id)}/name", "w") as f:
+                f.write(name)
+            return os.listdir(f"guilds/{gid}/{str(id)}")
+
+    def fetch_channels(gid):
+        return os.listdir(f"guilds/{gid}")
 
 
 def write_log(gid, cid, message):
-    messages = [int(m.split(".")[0]) for m in fetch_channel(gid, cid)]
-    if fetch_channel(gid, cid) == [] or max(messages)+1000 < int(datetime.datetime.now().strftime('%H%M%S')):
+    messages = [int(m.split(".")[0])
+                for m in channels.fetch_channel(gid, cid)].remove("name")
+    if messages == [] or max(messages)+1000 < int(datetime.datetime.now().strftime('%H%M%S')):
         with open(f"guilds/{gid}/{cid}/{datetime.datetime.now().strftime('%H%M%S')}.txt", "w") as f:
             try:
                 f.write(
