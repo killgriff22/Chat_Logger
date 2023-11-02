258df00aba8075be5fae5ead0dc573b764ed8c5c
killgriff22 <kilgrif22033@gmail.com>
2023-11-02 14:07:21 -0400

    Fixed Config.py to allow for parsing of logs!

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import discord
 import os
 import datetime
+import re
 client = discord.Client()
-TOKEN = ""
+TOKEN = 
 
 
 class guilds:
-    def fetch_guild(id, name):
+    def fetch_guild(id, name=""):
         if str(id) in os.listdir("guilds"):
             return os.listdir(f"guilds/{str(id)}")
         else:
@@ -20,22 +21,25 @@ class guilds:
 
 
 class channels:
-    def fetch_channel(gid, id, name):
-        if str(id) in guilds.fetch_guild(gid):
-            return os.listdir(f"guilds/{gid}/{str(id)}")
+    def fetch_channel(gid, id, name="", gname=None):
+        if str(id) in guilds.fetch_guild(gid, gname if gname else None):
+            return os.listdir(f"guilds/{gid}/{str(id)}").remove("name") if not os.listdir(f"guilds/{gid}/{str(id)}").remove("name") == None else []
         else:
             os.mkdir(f"guilds/{gid}/{str(id)}")
             with open(f"guilds/{gid}/{str(id)}/name", "w") as f:
                 f.write(name)
-            return os.listdir(f"guilds/{gid}/{str(id)}")
+            return os.listdir(f"guilds/{gid}/{str(id)}").remove("name") if not os.listdir(f"guilds/{gid}/{str(id)}").remove("name") == None else []
 
     def fetch_channels(gid):
         return os.listdir(f"guilds/{gid}")
 
 
 def write_log(gid, cid, message):
+
+    print(channels.fetch_channel(
+        gid, cid, re.sub(r'\W+', '', message.channel.name), re.sub(r'\W+', '', message.guild.name)))
     messages = [int(m.split(".")[0])
-                for m in channels.fetch_channel(gid, cid)].remove("name")
+                for m in channels.fetch_channel(gid, cid)]
     if messages == [] or max(messages)+1000 < int(datetime.datetime.now().strftime('%H%M%S')):
         with open(f"guilds/{gid}/{cid}/{datetime.datetime.now().strftime('%H%M%S')}.txt", "w") as f:
             try:
