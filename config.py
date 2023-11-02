8c31cdcbaad9b2fe9e389aa913a7bb9e92d8b710
killgriff22 <kilgrif22033@gmail.com>
2023-11-02 15:41:29 -0400

    moved imports, client declatartion, and log functions to config

--- config.py
+++ config.py
@@ -1,1 +1,48 @@
+import discord
+import os
+import datetime
+client = discord.Client()
 TOKEN = ""
+
+
+def fetch_guild(id):
+    if str(id) in os.listdir("guilds"):
+        return os.listdir(f"guilds/{str(id)}")
+    else:
+        os.mkdir(f"guilds/{str(id)}")
+        return os.listdir(f"guilds/{str(id)}")
+
+
+def fetch_channel(gid, id):
+    if str(id) in fetch_guild(gid):
+        return os.listdir(f"guilds/{gid}/{str(id)}")
+    else:
+        os.mkdir(f"guilds/{gid}/{str(id)}")
+        return os.listdir(f"guilds/{gid}/{str(id)}")
+
+
+def write_log(gid, cid, message):
+    messages = [int(m.split(".")[0]) for m in fetch_channel(gid, cid)]
+    if fetch_channel(gid, cid) == [] or max(messages)+1000 < int(datetime.datetime.now().strftime('%H%M%S')):
+        with open(f"guilds/{gid}/{cid}/{datetime.datetime.now().strftime('%H%M%S')}.txt", "w") as f:
+            try:
+                f.write(
+                    f"{message.id}:{message.author.id}:{message.author.name}:{message.content}:{datetime.datetime.now().strftime('%H%M%S')}")
+            except:
+                print(
+                    f"Could Not Log {message.id}:{message.content}:{datetime.datetime.now().strftime('%H%M%S')}")
+    else:
+        with open(f"guilds/{gid}/{cid}/{max(messages)}.txt", "a") as f:
+            try:
+                f.write(
+                    f"\n{message.id}:{message.author.id}:{message.author.name}:{message.content}:{datetime.datetime.now().strftime('%H%M%S')}")
+            except:
+                print(
+                    f"Could Not Log {message.id}:{message.content}:{datetime.datetime.now().strftime('%H%M%S')}")
+
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user.name}")
+    print(f"ID: {client.user.id}")
+    print("------")
--- main.py
+++ main.py
@@ -1,46 +1,5 @@
-import discord
-import os
-import datetime
 from config import *
-client = discord.Client()
 Logging = True
-
-
-def fetch_guild(id):
-    if str(id) in os.listdir("guilds"):
-        return os.listdir(f"guilds/{str(id)}")
-    else:
-        os.mkdir(f"guilds/{str(id)}")
-        return os.listdir(f"guilds/{str(id)}")
-
-
-def fetch_channel(gid, id):
-    if str(id) in fetch_guild(gid):
-        return os.listdir(f"guilds/{gid}/{str(id)}")
-    else:
-        os.mkdir(f"guilds/{gid}/{str(id)}")
-        return os.listdir(f"guilds/{gid}/{str(id)}")
-
-
-def write_log(gid, cid, message):
-    messages = [int(m.split(".")[0]) for m in fetch_channel(gid, cid)]
-    if fetch_channel(gid, cid) == [] or max(messages)+1000 < int(datetime.datetime.now().strftime('%H%M%S')):
-        with open(f"guilds/{gid}/{cid}/{datetime.datetime.now().strftime('%H%M%S')}.txt", "w") as f:
-            try:
-                f.write(
-                    f"{message.id}:{message.author.id}:{message.author.name}:{message.content}:{datetime.datetime.now().strftime('%H%M%S')}")
-            except:
-                print(
-                    f"Could Not Log {message.id}:{message.content}:{datetime.datetime.now().strftime('%H%M%S')}")
-    else:
-        with open(f"guilds/{gid}/{cid}/{max(messages)}.txt", "a") as f:
-            try:
-                f.write(
-                    f"\n{message.id}:{message.author.id}:{message.author.name}:{message.content}:{datetime.datetime.now().strftime('%H%M%S')}")
-            except:
-                print(
-                    f"Could Not Log {message.id}:{message.content}:{datetime.datetime.now().strftime('%H%M%S')}")
-
 
 async def check_command(message):
     if not message.author == client.user:
@@ -73,14 +32,9 @@ async def check_command(message):
             for letter in message.content.split(" ")[1].lower():
                 await target_message.add_reaction(emojis[letters.index(letter)])
 
-
-@client.event
-async def on_ready():
-    print(client.user.name)
-
-
 @client.event
 async def on_message(message):
     # write_log(message.guild.id, message.channel.id, message)
     await check_command(message)
+
 client.run(TOKEN)
